8d4f47ca7b49282476116d21bd856cb405b1613c
AlexKorzhov <half_life@tut.by>
2014-11-13 10:33:56 +0300

    Commit and push from IDEA

--- src/com/git/test/Runner.java
+++ src/com/git/test/Runner.java
@@ -7,5 +7,6 @@ public class Runner {
     public static void main(String args[]) {
         System.out.println("Hello git");
         System.out.println("To be or not to be.");
+        System.out.println("Try commit and push from IDEA");
     }
 }
